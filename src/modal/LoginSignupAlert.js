9f9f2a3687d721141442bab4eae5707d88d8b838
Ritik Dauthal <ritikdauthal12345@gmail.com>
2023-09-16 20:29:10 +0530

    Alert design

--- src/modal/LoginSignupAlert.js
+++ src/modal/LoginSignupAlert.js
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { alertContext } from "../App"
+import { Icon } from "@iconify/react"
 
 export default function LoginSignupAlert() {
     const { setLoginSignupalert, LoginSignupalertMessage } = useContext(alertContext)
@@ -9,12 +10,13 @@ export default function LoginSignupAlert() {
         <div className="fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-gray-300 bg-opacity-70"
             style={{ zIndex: 1000 }}>
             <div className='p-8 rounded-lg md:w-1/4 w-[90%] bg-white'>
-                <div className='flex flex-col items-center'>
-                    <div className="mb-5 text-lg font-semibold text-black">
-                        ESpotify Says:
+                <div className='flex flex-col items-center space-y-2'>
+                    <div className='flex items-center space-x-1 h-fit' >
+                        <Icon icon="logos:spotify-icon" width="25" />
+                        <p className='text-lg font-medium text-black'>Spotify</p>
                     </div>
                     <div className='text-green-600'>{LoginSignupalertMessage}</div>
-                    <button className='px-2 py-1 mt-2 text-black bg-green-600 rounded-md' onClick={() => setLoginSignupalert(false)}>OK</button>
+                    <button className='px-2 py-1 text-black bg-green-600 rounded-md' onClick={() => setLoginSignupalert(false)}>OK</button>
                 </div>
             </div>
         </div>
