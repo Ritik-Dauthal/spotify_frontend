944939b70dbfa83a75ce4e3f9e1e51a82c4ebdec
Ritik Dauthal <ritikdauthal12345@gmail.com>
2023-09-17 20:55:36 +0530

    Card2 /login

--- src/Components/Shared/Card2.js
+++ src/Components/Shared/Card2.js
@@ -1,11 +1,24 @@
 import { Icon } from "@iconify/react";
 import { useContext } from "react";
 import songContext from "../../SongContext";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Card2 = (item) => {
     const { currentSong, setCurrentSong } = useContext(songContext);
+    const [cookie, setCookie, removeCookie] = useCookies(["token"]);
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        if (cookie.token) {
+            setCurrentSong(item.item);
+        } else {
+            navigate('/login');
+        }
+    };
+
     return (
-        <div className="flex flex-col w-full h-48 p-4 bg-black rounded-lg cursor-pointer" onClick={() => setCurrentSong(item.item)}>
+        <div className="flex flex-col w-full h-48 p-4 bg-black rounded-lg cursor-pointer" onClick={handleClick}>
             <div className="h-3/5">
                 <img
                     className="object-cover w-full h-full rounded-md"
